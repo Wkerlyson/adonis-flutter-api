a559aac014a7ba17a75f64bb87de17910f53e311
Wkerlyson Batista <wkerlyson.batista@uece.br>
2024-03-14 18:55:32 -0300

    first commit

---
+++ start/routes.ts
@@ -0,0 +1,40 @@
+import { HttpContext } from '@adonisjs/core/http';
+import User from '#models/user'
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js';
+
+router.get('/', async () => {
+
+  const user = new User()
+
+  user.fullName = 'Wkerlyson'
+  user.email = 'wk@email.com'
+  user.password = '123456'
+  user.tipo = 'admin'
+
+  await user.save()
+
+  return {
+    id: user.id,
+  }
+})
+
+router.post('/get-token', async ({request}: HttpContext) => {
+  const { email, password } = request.only(['email', 'password'])
+  const user = await User.verifyCredentials(email, password)
+
+  const token = await User.accessTokens.create(user)
+
+  return {
+    type: 'bearer',
+    value: token.value!.release(),
+  }
+})
+
+router.get('/auth', async ({response}) => {
+  return response.ok('Usuario autenticado: acesso autorizado')
+}).use(middleware.auth({
+  guards: ['api']
+}))
+
+
